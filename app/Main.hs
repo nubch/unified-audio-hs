aa2838ad8cd69530cc8b8be6e2625464bfc1ece5
Keksgurke <nubch2@gmail.com>
2025-06-19 18:44:56 +0200

    added example main

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@
 
 module Main where
 
-import Effectful ( runEff )
+import Effectful
 import Effectful.Dispatch.Static ( unsafeEff_ )
 import Control.Concurrent (threadDelay)
 import Audio ( playSound, stopSound, setVolume, setPanning )
@@ -14,13 +14,31 @@ import qualified Fmod.Fmod as Fmod
 import qualified Interface as I
 
 main :: IO ()
-main = runEff $ SDL.runAudio do
-  channel <- playSound @SDL.Channel "flim.mp3"
-  unsafeEff_ $ threadDelay (3 * 1000000)
-  _ <- setPanning channel (I.mkPanning (-1.0))
-  unsafeEff_ $ threadDelay (3 * 1000000)
-  _ <- setPanning channel (I.mkPanning (1.0))
-  unsafeEff_ $ threadDelay (3 * 1000000)
-  _ <- stopSound channel
-  pure ()
+main = runEff $ Fmod.runAudio do
+  -- Play three sounds
+  ch0 <- playSound @Fmod.Channel "flim.mp3"
+  ch1 <- playSound @Fmod.Channel "flim.mp3"
+  ch2 <- playSound @Fmod.Channel "flim.mp3"
 
+  -- Pan ch1 completely to the left 
+  delaySec 3
+  setPanning ch1 (I.mkPanning (-1.0))
+
+  -- Pan ch1 completely to the right
+  delaySec 3
+  stopSound ch1
+  setPanning ch2 (I.mkPanning 1.0)
+
+  -- Lower volume of ch0 
+  delaySec 3
+  stopSound ch1
+  stopSound ch2
+  setVolume ch0 (I.mkVolume (0.2))
+
+  delaySec 3
+  stopSound ch0
+
+  pure ()
+  where
+    delaySec :: Int -> Eff es ()
+    delaySec s = unsafeEff_ $ threadDelay (s * 1000000)
