a0f1e4896c8fc5bb2c59f2e0721125099a1ffd3d
Keksgurke <nubch2@gmail.com>
2025-06-23 20:41:12 +0200

    added effectful plugin

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
@@ -14,30 +17,15 @@ import qualified Fmod.Fmod as Fmod
 import qualified Interface as I
 
 main :: IO ()
-main = runEff $ Fmod.runAudio do
-  -- Play three sounds
-  ch0 <- playSound @Fmod.Channel "flim.mp3"
-  ch1 <- playSound @Fmod.Channel "flim.mp3"
-  ch2 <- playSound @Fmod.Channel "flim.mp3"
+main = runEff $ SDL.runAudio test
+
+test :: (I.AudioEffect channel :> es) => Eff es ()
+test = do
+  ch0 <- playSound "flim.mp3"
 
   -- Pan ch1 completely to the left 
   delaySec 3
-  setPanning ch1 (I.mkPanning (-1.0))
-
-  -- Pan ch1 completely to the right
-  delaySec 3
-  stopSound ch1
-  setPanning ch2 (I.mkPanning 1.0)
-
-  -- Lower volume of ch0 
-  delaySec 3
-  stopSound ch1
-  stopSound ch2
-  setVolume ch0 (I.mkVolume (0.2))
-
-  delaySec 3
-  stopSound ch0
-
+  setPanning ch0 (I.mkPanning (-1.0))
   pure ()
   where
     delaySec :: Int -> Eff es ()
